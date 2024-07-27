79dfa01879fb9d2c47e17728034136e24f5483e5
Thiago Bezerra <thiagobezerra.dev@gmail.com>
2024-07-27 04:57:30 -0300

    fix 'broken links' error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
           items: [
             {
               label: 'Guias',
-              to: '/docs/intro',
+              to: '/docs/introducao',
             },
           ],
         },
